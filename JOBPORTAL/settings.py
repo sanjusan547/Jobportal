27fecd73756e50afb68096224092457afd5e7e9b
sanju <sanjusan547@gmail.com>
2025-07-21 21:14:29 +0530

    added decouple

--- JOBPORTAL/settings.py
+++ JOBPORTAL/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u7n=o86p9!drqg9pa5_akbch627%z=!v9r!3uoa8ttnx7jta3m'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,11 +82,17 @@ WSGI_APPLICATION = 'JOBPORTAL.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'default': {
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': config('DATABASE_NAME'),
+      'USER':config('DATABASE_USER'),
+      'PASSWORD':config('DATABASE_PASSWORD'),
+      'HOST':'localhost',
+      'PORT':'5432',
     }
 }
+
+
 
 
 # Password validation
@@ -161,6 +168,6 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'poppysayana@gmail.com'
-EMAIL_HOST_PASSWORD = 'kxkzphzxpbrxwwrn'  # Use an App Password for Gmail
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # Use an App Password for Gmail
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
